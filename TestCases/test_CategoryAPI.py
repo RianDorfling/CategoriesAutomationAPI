78c038c57e84f643cbb7748245d03d1937000a3f
RianDorfling <rian.dorfling@gmail.com>
2022-05-16 17:22:32 +0200

    First checkin

---
+++ TestCases/test_CategoryAPI.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import jsonpath
+import jsonpath_rw_ext as jp
+
+url = 'https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false'
+
+
+def test_name():
+    response = requests.get(url)
+    json_response = json.loads(response.text)
+    name = jsonpath.jsonpath(json_response, 'Name')
+    name1 = ['Carbon credits']
+    assert name == name1, "name does not match"
+    assert name is not None
+
+
+def test_canrelist():
+    response = requests.get(url)
+    json_response = response.json()
+    assert json_response['CanRelist'] is True
+
+
+def test_promotions():
+    response = requests.get(url)
+    json_r = json.loads(response.text)
+    for name in jp.match("Promotions[*].Name", json_r):
+        if name == "Gallery":
+            assert name == "Gallery", "Name don't exist"
+            for description in jp.match("Promotions[?(@.Description=='Good position in category')].Description",
+                                        json_r):
+                if description == "Good position in category":
+                    assert description == "Good position in category", "Description don't exist "
